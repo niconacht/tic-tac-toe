87d5f71203aec79b6ca9b813f165bd21539921d5
Nicoletta <nicoletta.hirsch@gmail.com>
2020-05-11 13:00:10 +0200

    structure

---
+++ script.js
@@ -0,0 +1,60 @@
+
+//GAMEBOARD
+
+
+
+
+
+
+
+
+//PLAYERS
+
+
+
+
+
+
+
+//GAME
+
+    //forEach .cell addeventlistener click
+    //has cell been clicked?
+        //update UserInterface 
+        //update gameState (array)
+
+
+
+
+
+//SETTINGS
+
+
+
+
+
+
+
+// const displayController = (() => {
+//     const
+
+//     //using revealing module pattern and return anonymous object with pointers to the private functionalities
+//     return {
+
+//     }
+// })();
+
+
+
+
+// const playerModule = (name, symbol) => {
+//     return {name, symbol};
+// }
+
+// const gameboardModule = (() => {
+//     const gameboard = [];
+
+//     return {
+
+//     }
+// })();
