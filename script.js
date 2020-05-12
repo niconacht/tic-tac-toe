e407c4681d785aa13c40be1ce3fa630037c6feab
Nico Nacht <nd.hagedorn@gmail.com>
2020-05-12 06:29:54 +0200

    progress

--- script.js
+++ script.js
@@ -1,60 +1,48 @@
+'use strict';
 
-//GAMEBOARD
+//Playerfactory
+function newPlayer (type, active) {
+    return {
+        type,
+        active,
+    }
+}
 
+//Game_logic
+const playGame = (function(e) {
 
+    let gameProgress = [];
+    const player1 = newPlayer("X", true);
+    const player2 = newPlayer("O", false);
+    let activePlayer = player1;
+    const changePlayer = function(player1, player2) {
+        return (activePlayer = activePlayer.type === "X"?  player2 : player1);
+    }
+     
+    return {
+       changeCell : function(e) {
+           e.target.textContent = activePlayer.type;
+            gameProgress.push([activePlayer, e.target.id]);
+            changePlayer(player1, player2);
+       }
 
+    }
 
-
-
-
-
-//PLAYERS
-
-
-
-
-
-
-
-//GAME
-
-    //forEach .cell addeventlistener click
-    //has cell been clicked?
-        //update UserInterface 
-        //update gameState (array)
-
-
-
-
-
-//SETTINGS
-
-
-
-
-
-
-
-// const displayController = (() => {
-//     const
-
-//     //using revealing module pattern and return anonymous object with pointers to the private functionalities
-//     return {
-
-//     }
-// })();
+})();
+//array that keeps track of what has been clicked
 
 
 
 
-// const playerModule = (name, symbol) => {
-//     return {name, symbol};
-// }
 
-// const gameboardModule = (() => {
-//     const gameboard = [];
 
-//     return {
 
-//     }
-// })();
+
+
+//Eventhandlers
+//document.addEventListener("DOMContentLoaded", startGame);
+
+const container = document.getElementById("gameboard");
+container.addEventListener("click",playGame.changeCell);
+
+
